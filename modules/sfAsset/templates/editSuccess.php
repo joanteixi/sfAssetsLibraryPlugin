c2688e93030647d705e3c3bb9f97ccf0e09538eb
joanteixido <joan@laiogurtera.com>
2010-10-18 11:26:17 +0200

    Al obrir un pdf o doc (no jpg) no apareix la vista prèvia

--- modules/sfAsset/templates/editSuccess.php
+++ modules/sfAsset/templates/editSuccess.php
@@ -2,19 +2,14 @@
 <?php if($sf_asset->isImage()) : ?>
     <?php include_partial('edit_asset', array('sf_asset' => $sf_asset)); ?>
 <?php else : ?>
-<div class="foto_details">
-  <div class="asset_left">
-    <?php echo asset_image_tag($sf_asset, 'large',array('id' => 'original'),null, true) ?>
-  </div>
 
-  <div class="descripcio_block">
+  <div class="descripcio">
     <p><strong>Fitxer:</strong> <?php echo auto_wrap_text($sf_asset->getFilename())?></p>
 
  <?php include_partial('edit_descripcio', array('sf_asset' => $sf_asset));?>
     <div class="clear"></div>
     <p><?php echo link_to('Obre el document',$sf_asset->getUrl());?></p>
     <p><?php echo __('Created on %date%', array('%date%' => format_date($sf_asset->getCreatedAt('U'))), 'sfAsset') ?></p>
-</div>
 </div>
 
 <?php endif ?>
